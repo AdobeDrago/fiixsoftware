10903ae49a42ab425ea3d07be53865e887e1ac8d
Harishreddyneelapu <N120759@icf.com>
2026-09-01 19:03:12 +0530

    Update blog listing to reflect a change in the number of posts displayed per page from 16 to 9.

--- blocks/blog-listing/blog-listing.js
+++ blocks/blog-listing/blog-listing.js
@@ -17,7 +17,7 @@ const CATEGORIES = [
 
 const LISTING_PATHS = new Set(['/blog', '/blog/', '/blog.html']);
 
-/* production lists 16 posts per page on /blog/ (see fiixsoftware.com/blog) */
+/* production lists 9 posts per page on /blog/ (see fiixsoftware.com/blog) */
 const PAGE_SIZE = 9;
 
 function slugifyCategory(name) {
